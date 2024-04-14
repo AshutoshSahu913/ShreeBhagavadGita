a5a85816df5e0d159e59968fff9211589fa8c08a
Ashutosh <1ashutoshsahu@gmail>
2024-04-14 10:36:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shreebhagavadgita/View/fragments/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.shreebhagavadgita.View.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.shreebhagavadgita.R
+import com.example.shreebhagavadgita.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+
+
+
+        return binding.root
+    }
+
+    companion object {
+    }
+}
---
+++ app/src/main/java/com/example/shreebhagavadgita/View/fragments/SplashFragment.kt
@@ -0,0 +1,53 @@
+package com.example.shreebhagavadgita.View.fragments
+
+import android.R.color
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.shreebhagavadgita.R
+
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        setStatusBarColor()
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            }, 5000
+        )
+
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+
+    }
+
+    private fun setStatusBarColor() {
+        val window = activity?.window
+        window?.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.appColor)
+        window?.let {
+            WindowCompat.getInsetsController(it, window.decorView).apply {
+                isAppearanceLightStatusBars = true
+            }
+        }
+    }
+
+    companion object {
+    }
+}
